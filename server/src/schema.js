45f2d56f8e95dc209ea94a3a4fe6a4c3c95bf7e8
Anthony Barnes <abarnes204@aol.com>
2022-04-27 16:41:50 -0400

    added gql query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,7 +1,16 @@
 const {gql} = require('apollo-server');
 
 const typeDefs = gql`
-    #Schema definitions go here
+    """
+    The Query type is defined like any other object type
+    it references types created in the schema below
+    """
+    type Query {
+    "Get tracks array for homepage grid"
+        tracksForHome: [Track!]!
+    }
+
+    #Schema definitions go here:
 
     "A track is a group of Modules that teaches about a specific topic"
     type Track {
