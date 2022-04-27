a4a7948074b2594b6c09e1fc47953882f83ec4d4
Anthony Barnes <abarnes204@aol.com>
2022-04-27 16:51:52 -0400

    create mock data object

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,32 @@
 const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({typeDefs});
+const server = new ApolloServer({
+    typeDefs,
+    mocks
+});
+
+/*
+The mocks object below contains functions that provide the mocked data 
+we want the server to return for each queried field.
+*/
+const mocks = {
+    Track: () => ({
+      id: () => 'track_01',
+      title: () => 'Astro Kitty, Space Explorer',
+      author: () => {
+        return {
+          name: 'Grumpy Cat',
+          photo:
+            'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
+        };
+      },
+      thumbnail: () =>
+        'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+      length: () => 1210,
+      modulesCount: () => 6
+    })
+  };
 
 server.listen().then(() => {
     console.log(`
