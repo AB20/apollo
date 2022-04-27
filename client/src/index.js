4bb1ef0d2a3125cff1d57cec118fdd528d43239e
Anthony Barnes <abarnes204@aol.com>
2022-04-27 18:56:22 -0400

    import symbols into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 ReactDOM.render(
   <React.StrictMode>
