a3ee56155360032b80f00bab40f529179910b890
Anthony Barnes <abarnes204@aol.com>
2022-04-27 19:23:29 -0400

    create getTracks gql query - tracks.js

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Layout } from '../components';
+import {gql} from '@apollo/client';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+ const TRACKS = gql`
+ # Query goes here
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        photo
+        name
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
